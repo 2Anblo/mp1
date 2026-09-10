8d8b61d8c1d6b7a2ae03b10b5cbec42d9ea75dbd
2Anblo <1542988920@qq.com>
2026-09-09 19:22:35 -0500

    add indicator

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,24 @@
 /* Your JS here. */
-console.log('Hello World!')
+const sections = document.querySelectorAll("section")
+const navLinks = document.querySelectorAll(".navbar__items a")
+const navbar = document.querySelector(".navbar");
+const navbarHeight = navbar.offsetHeight;
+
+window.addEventListener("scroll", () => {
+  let currentSection = "";
+
+  sections.forEach((section) => {
+    const sectionTop = section.offsetTop;
+
+    if (window.scrollY + navbarHeight >= sectionTop) {
+      currentSection = section.id;
+    }
+  });
+
+  navLinks.forEach((link) => {
+    link.classList.toggle(
+      "active",
+      link.getAttribute("href") === `#${currentSection}`
+    );
+  });
+});
