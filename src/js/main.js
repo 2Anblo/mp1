085897667570c19a3e85d5bcaf79aa28ab33722c
2Anblo <1542988920@qq.com>
2026-09-11 13:54:34 -0500

    add modals

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,15 @@
 /* Your JS here. */
-const sections = document.querySelectorAll("section")
-const navLinks = document.querySelectorAll(".navbar__items a")
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll(".navbar__items a");
 const navbar = document.querySelector(".navbar");
 const navbarHeight = navbar.offsetHeight;
-const track = document.querySelector(".about__carousel-track")
-const images = document.querySelectorAll(".about__carousel-track img")
-const leftButton = document.querySelector(".about__carousel-button--left")
-const rightButton = document.querySelector(".about__carousel-button--right")
-
+const track = document.querySelector(".about__carousel-track");
+const images = document.querySelectorAll(".about__carousel-track img");
+const leftButton = document.querySelector(".about__carousel-button--left");
+const rightButton = document.querySelector(".about__carousel-button--right");
+const modals = document.querySelectorAll(".modal");
+const cards = document.querySelectorAll(".services__card");
+const closeButtons = document.querySelectorAll(".modal__close");
 
 // section indicator
 window.addEventListener("scroll", () => {
@@ -60,4 +62,29 @@ leftButton.addEventListener("click", ()=>{
     currentIndex = images.length - 1;
   }
   updateCarousel();
+})
+
+
+// modal interaction
+cards.forEach((card) => {
+  card.addEventListener("click", ()=>{
+    const modalId = card.dataset.modal;
+    const modal = document.getElementById(modalId);
+
+    modal.classList.add("active");
+  });
+});
+
+closeButtons.forEach((button) => {
+  button.addEventListener("click", ()=>{
+    button.closest(".modal").classList.remove("active")
+  })
+})
+
+modals.forEach((modal) =>{
+  modal.addEventListener("click", ()=>{
+    if (event.target == modal){
+      modal.classList.remove("active")
+    }
+  })
 })
